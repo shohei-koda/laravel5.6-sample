6ed8978fc82ce7c5558aec6477fa40353c50ddbd
shohei-koda <30883132+shohei-koda@users.noreply.github.com>
2018-08-15 15:11:08 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #f00;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
